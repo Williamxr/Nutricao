bc9da7dca9976a47321e68a9467e8a2b08738c4b
William <william.teletabs@hotmail.com>
2022-02-16 15:09:28 -0300

    Criando var que mostra o resultado do imc calculado

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,17 @@
- var titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo"); // Como selecionar uma classe para mudar seu nome.
+titulo.textContent = "Nutricionista Willx"      //Pegando o texto da var(titulo) e alterando seu nome.
 
-titulo.textContent = "Meu artista favorito"
+var paciente = document.querySelector("#primeiro-paciente"); //pega a tr toda
+
+var tdPeso = paciente.querySelector(".info-peso"); // pega a td toda
+var peso = tdPeso.textContent; //pegando só o valor do peso do paciente
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+
+var imc = peso / (altura * altura); //calculando o imc
+
+tdImc.textContent = imc; //exibindo o resultado do imc calculado

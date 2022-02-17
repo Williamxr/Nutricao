f5e80feea10763ec4899a92bbf5b0bb1a0ae9a58
William <william.teletabs@hotmail.com>
2022-02-17 17:20:22 -0300

    Criando nova tr com td's para adicionar na tabela da página web

--- js/principal.js
+++ js/principal.js
@@ -36,9 +36,37 @@ var paciente = pacientes[i];
     }
 }
 
-var botaoAdd = document.querySelector("#adicionar-paciente");
-botaoAdd.addEventListener("click", function(event){
-    event.preventDefault()
-    console.log("daew");
-})
+var botaoAdd = document.querySelector("#adicionar-paciente"); // selecionando id 
+botaoAdd.addEventListener("click", function(event){      // adicionando escutador de eventos e criando função anonima preventDefalut
+    event.preventDefault();
 
+    var form = document.querySelector("#form-adiciona"); // selecionando o form
+
+    var nome = form.nome.value; // pegando os dados do form
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr"); //criando uma tr
+    
+    var nomeTd = document.createElement("td"); // criando td
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome; //preenchendo as td's com os valores que peguei do form ali em cima(.value)
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd); //colocando as td's dentro da tr
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr); // trazendo a tabela criada para a tabela já existente na página
+
+});

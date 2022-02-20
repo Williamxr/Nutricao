3300570e1c4d78a3fa23b221183aad6abf8100d5
William <william.teletabs@hotmail.com>
2022-02-19 22:44:38 -0300

    Adicionando classe para todos os dados do paciente que vai ser adicionado na tabela

--- js/form.js
+++ js/form.js
@@ -29,14 +29,18 @@ function obtemPacienteDoForm(form){
 function montaTr(paciente){
 
     var pacienteTr = document.createElement("tr");
-
     pacienteTr.classList.add("paciente");
 
     var nomeTd = document.createElement("td"); // criando td
+    nome.Td.classList.add("info-nome");
     var pesoTd = document.createElement("td");
+    nome.Td.classList.add("info-peso");
     var alturaTd = document.createElement("td");
+    nome.Td.classList.add("info-altura");
     var gorduraTd = document.createElement("td");
+    nome.Td.classList.add("info-gordura");
     var imcTd = document.createElement("td");
+    nome.Td.classList.add("info-imc");
 
     nomeTd.textContent = paciente.nome; //preenchendo as td's com os valores que peguei do form ali em cima(.value)
     pesoTd.textContent = paciente.peso;

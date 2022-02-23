9aef10447bfd42d461d55f8f4990fcecd4e6f3cc
William <william.teletabs@hotmail.com>
2022-02-22 22:36:48 -0300

    Criando span com mensagem de erro caso seja adicinado um paciente com dados inválidos

--- js/form.js
+++ js/form.js
@@ -8,8 +8,12 @@ botaoAdd.addEventListener("click", function(event){      // adicionando escutado
     
     var pacienteTr = montaTr(paciente);
 
-    if(!validaPaciente(paciente)){
-        return;
+    var erro = validaPaciente(paciente);
+
+    if(erro.length > 0){
+        var mensagemErro = document.querySelector("#mensagem-erro");
+            mensagemErro.textContent = erro;
+            return;
     }
  
     var tabela = document.querySelector("#tabela-pacientes");
@@ -58,9 +62,9 @@ function montaTd(dado, classe){
 function validaPaciente(paciente){
 
     if(validaPeso(paciente.peso)){
-        return true;
+        return "";
     }else{
-        return false;
+        return "O peso é inválido";
     }
 
 }

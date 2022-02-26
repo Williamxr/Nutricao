7675c82a7bbcfba73726b243cbdcfc64867f8415
William <william.teletabs@hotmail.com>
2022-02-25 23:36:32 -0300

    Criando função para animar a remoção do paciente da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,10 +3,12 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
-    paiDoAlvo.remove();
-})
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+});
 
 //pacientes.forEach(function(paciente){
 //    paciente.addEventListener("dblclick", function(){

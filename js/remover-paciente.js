55eb4a3392def48e15b731f7bcb5fb9e2783fe33
William <william.teletabs@hotmail.com>
2022-02-25 23:29:00 -0300

    Criando função para remover novos pacientes que são adicionados na tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,15 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    })
-});
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; //tr = paciente = remover
+    paiDoAlvo.remove();
+})
+
+//pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+//        this.remove();
+//    })
+//});

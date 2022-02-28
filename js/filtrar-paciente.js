cbb516ac373d0ebba90464332ed44a14755487b7
William <william.teletabs@hotmail.com>
2022-02-28 19:37:36 -0300

    Testando a expressão para buscar por pacientes

--- js/filtrar-paciente.js
+++ js/filtrar-paciente.js
@@ -10,7 +10,7 @@ campoFiltro.addEventListener("input", function(){
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
             var expressao = new RegExp(this.value, "i");
-            if (nome != this.value){
+            if (!expressao.test(nome)){
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");

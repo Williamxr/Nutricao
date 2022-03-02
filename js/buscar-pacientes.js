cac3082b9fc80c449b358eae1625e541e288ea26
William <william.teletabs@hotmail.com>
2022-03-01 23:30:27 -0300

    Criando eventListener no meu xrh, para que quando a resposta estiver carregada (load), trazer a lista de pacientes para minha tabela

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -1,5 +1,15 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
 botaoAdicionar.addEventListener("click", function(){
-    console.log("daew");
-})
+   
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");     //qual requisição vai fazer e para onde
+
+    xhr.addEventListener("load", function(){
+        
+    });
+
+    xhr.send();
+
+});

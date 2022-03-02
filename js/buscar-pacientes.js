ff4020fd14b0392a900d4c5d361e907ce2a5d97e
William <william.teletabs@hotmail.com>
2022-03-02 15:15:45 -0300

    Criando verificação para que se algo der errado na requisição, o erro seja mostrado para o usuário no console.log

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -7,12 +7,20 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");     //qual requisição vai fazer e para onde
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta)
+        
+        if(xhr.status == 200){
+                    
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta)
 
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+
+        }else{
+            console.log(xhr.status);
+            console.log(xrh.responseText);
+        }
 
     });
 
